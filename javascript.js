f490c94c8fbe689994571fdf3875b16349953ab6
tg <157854871+lilyponds@users.noreply.github.com>
2024-03-17 23:56:01 -0500

    Add functionality to ask for number of rounds and return a final winner/Loser with Score

--- javascript.js
+++ javascript.js
@@ -5,24 +5,53 @@ let options = [
     'scissor'
 ];
 
-let computer = computerChoice(options);
+let gameSize = prompt("How many games do you want to play?", 3);
+let finalScore = play(options,gameSize);
+console.log(finalScore);
+console.log("*-----------GAME OVER------------*");
+
+
+function play(options,size){
+    let scoreComp = 0;
+    let scoreHum = 0;
+    let message;
+
+for(i=1;i<=size;i++) {
+    let computer = computerChoice(options);
 let human = prompt("Input your selection rock, paper or scissor:",'rock');
-let output = userInputResultCheck(human);
+let output = userInputResultCheck(human,computer);
+console.log(computer);
+console.log(human);
+console.log(output);
+if(output=="You Won"){
+    scoreHum +=1;
+}else if(output=="Computer Won"){
+    scoreComp+=1;
+}else {
+    scoreComp+=0;
+    scoreHum+=0;
+}
+}
 
-function userInputResultCheck(input){
+if(scoreComp>scoreHum){
+    message =  'Computer Won - Final Score Computer: ' + scoreComp + ' Human: ' + scoreHum;
+} else if(scoreComp<scoreHum){
+    message =  'You Won - Final Score Computer: ' + scoreComp +' Human: ' + scoreHum;
+} else {
+    message = 'It is a Tie - Final Score Computer: ' + scoreComp +' Human: ' + scoreHum;
+}
 
+return message;
+
+}
+
+function userInputResultCheck(input,computer){
     if(input.toLowerCase()==options[0] || input.toLowerCase()==options[1]|| input.toLowerCase()==options[2]){
     return whoWon(computer,input.toLowerCase()) ;
 } else {
      return "Select a Valid Choice Please!";
 }
 }
-
-console.log(computer);
-
-console.log(human);
-
-console.log(output);
 
 
 function computerChoice(choice){
@@ -32,7 +61,7 @@ function computerChoice(choice){
 
 function whoWon(computer, human){
     let result = [
-        "It's a Tie",
+        "Tie",
         "You Won",
         "Computer Won"
     ]
